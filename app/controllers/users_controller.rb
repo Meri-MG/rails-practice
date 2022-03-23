7bda642e813bd223ac9b1919e418d2e3c276c7cb
Meri <mgogi13@freeuni.edu.ge>
2022-03-23 19:50:24 +0400

    Add links on user pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Welcome to our blog #{@user.username}, you have successfully signed up!"
-      redirect_to articles_path
+      redirect_to users_path
     else
       render 'new', status: :unprocessable_entity 
     end
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Account was updated successfully."
-      redirect_to articles_path
+      redirect_to users_path
     else
       render 'edit', status: :unprocessable_entity 
     end
